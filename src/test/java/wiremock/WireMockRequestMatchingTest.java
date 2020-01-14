e192faa09657d9f5a32dda87fafa6a609a6600e2
Bas Dijkstra <bas@ontestautomation.com>
2020-01-14 13:26:05 +0100

    Added WireMock request matching examples

---
+++ src/test/java/wiremock/WireMockRequestMatchingTest.java
@@ -0,0 +1,48 @@
+package wiremock;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import io.restassured.http.ContentType;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static io.restassured.RestAssured.given;
+
+public class WireMockRequestMatchingTest {
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(9876);
+
+    public void createStubUsingRequestMatchingTechniques() {
+
+        stubFor(
+            get(
+                urlEqualTo("/request-matching")
+            )
+                .withHeader("Content-Type", containing("text/plain"))
+                .withCookie("myCookie", equalTo("chocolateChip"))
+                .withBasicAuth("username","password")
+                .willReturn(
+                    aResponse()
+                        .withStatus(200)
+                        .withBody("Your request satisfied all conditions.")));
+    }
+
+    @Test
+    public void invokeWireMockRequestMatchingStub_checkStatusCodeAndBody() {
+
+        createStubUsingRequestMatchingTechniques();
+
+        given().
+            auth().preemptive().basic("username","password").
+            contentType("text/plain").
+            cookie("myCookie","chocolateChip").
+        when().
+            get("http://localhost:9876/request-matching").
+        then().
+            assertThat().
+            statusCode(200).
+        and().
+            body(org.hamcrest.Matchers.equalTo("Your request satisfied all conditions."));
+    }
+}
