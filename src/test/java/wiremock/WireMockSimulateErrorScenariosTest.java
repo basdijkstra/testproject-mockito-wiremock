23d3ae3e5626b71cf2dd1a6056278c06a1f48310
Bas Dijkstra <bas@ontestautomation.com>
2020-01-28 13:22:44 +0100

    Added other examples for faulty behaviour

--- src/test/java/wiremock/WireMockSimulateErrorScenariosTest.java
+++ src/test/java/wiremock/WireMockSimulateErrorScenariosTest.java
@@ -1,9 +1,13 @@
 package wiremock;
 
+import com.github.tomakehurst.wiremock.http.Fault;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import io.restassured.http.ContentType;
+import org.apache.http.client.ClientProtocolException;
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static io.restassured.RestAssured.given;
@@ -37,5 +41,57 @@ public class WireMockSimulateErrorScenariosTest {
         then().
             assertThat().
             statusCode(503);
+    }
+
+    private void receivePostToCheckOut_ResponseDelayed() {
+
+        stubFor(
+            post(
+                urlEqualTo("/checkout")
+            )
+                .withHeader("Scenario", equalTo("Delay"))
+                .willReturn(
+                    aResponse()
+                        .withStatus(200)
+                        .withFixedDelay(2000)));
+    }
+
+    @Test
+    public void invokeWireMockDelayStub_checkStatusCodeAndResponseTime() {
+
+        receivePostToCheckOut_ResponseDelayed();
+
+        given().
+            header("Scenario", "Delay").
+        when().
+            post("http://localhost:9876/checkout").
+        then().
+            assertThat().
+            statusCode(200).
+        and().
+            time(org.hamcrest.Matchers.greaterThan(2000L), TimeUnit.MILLISECONDS);
+    }
+
+    private void receivePostToCheckOut_RespondWithGarbage() {
+
+        stubFor(
+            post(
+                urlEqualTo("/checkout")
+            )
+                .withHeader("Scenario", equalTo("FaultyBody"))
+                .willReturn(
+                    aResponse()
+                        .withFault(Fault.RANDOM_DATA_THEN_CLOSE)));
+    }
+
+    @Test(expected = ClientProtocolException.class)
+    public void invokeWireMockFaultyBodyStub_checkThrowsClientProtocolException() {
+
+        receivePostToCheckOut_RespondWithGarbage();
+
+        given().
+            header("Scenario", "FaultyBody").
+        when().
+            post("http://localhost:9876/checkout");
     }
 }
